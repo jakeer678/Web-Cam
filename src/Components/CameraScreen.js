af60421852647de8e37370d5e29bb6a3de5a97b7
jakeer678 <jakeersasanoor@gmail.com>
2023-06-13 13:44:38 +0530

    updated

--- src/Components/CameraScreen.js
+++ src/Components/CameraScreen.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect, useRef } from 'react';
 import Webcam from 'react-webcam';
+import './CameraScreen.css';
 
 const CameraScreen = () => {
   const [coordinates, setCoordinates] = useState({ latitude: 0, longitude: 0 });
   const [orientation, setOrientation] = useState({ alpha: 0, beta: 0, gamma: 0 });
+  const [photo, setPhoto] = useState(null);
   const webcamRef = useRef(null);
 
   useEffect(() => {
@@ -26,37 +28,58 @@ const CameraScreen = () => {
   useEffect(() => {
     const handleDeviceOrientation = (event) => {
       const { alpha, beta, gamma } = event;
-      console.log('Device Orientation:', event);
       setOrientation({ alpha, beta, gamma });
     };
 
-    if ('DeviceOrientationEvent' in window) {
-      window.addEventListener('deviceorientation', handleDeviceOrientation);
-    } else {
-      console.log('Device orientation events are not supported');
-    }
+    window.addEventListener('deviceorientation', handleDeviceOrientation);
 
     return () => {
       window.removeEventListener('deviceorientation', handleDeviceOrientation);
     };
   }, []);
 
+  useEffect(() => {
+    const handleOrientationChange = () => {
+      const updatedOrientation = {
+        alpha: orientation.alpha >= 0 ? orientation.alpha : 360 + orientation.alpha,
+        beta: orientation.beta >= 0 ? orientation.beta : 360 + orientation.beta,
+        gamma: orientation.gamma >= 0 ? orientation.gamma : 360 + orientation.gamma,
+      };
+      setOrientation(updatedOrientation);
+    };
+
+    window.addEventListener('orientationchange', handleOrientationChange);
+
+    return () => {
+      window.removeEventListener('orientationchange', handleOrientationChange);
+    };
+  }, [orientation]);
+
+  const capturePhoto = () => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setPhoto(imageSrc);
+  };
+
   return (
-    <div className="container">
+    <div className="camera-screen">
       <h1>Camera Screen</h1>
-      <div className="webcam-container">
-        <Webcam className="webcam-video" audio={false} ref={webcamRef} screenshotFormat="image/jpeg" />
+      <div className="camera-container">
+        <Webcam audio={false} ref={webcamRef} screenshotFormat="image/jpeg" className="camera" />
       </div>
-      <div className="coordinates">
+      <div className="info-container">
         <h2>Coordinates:</h2>
         <p>Latitude: {coordinates.latitude.toFixed(6)}</p>
         <p>Longitude: {coordinates.longitude.toFixed(6)}</p>
-      </div>
-      <div className="orientation">
         <h2>Orientation:</h2>
         <p>Alpha: {orientation.alpha ? orientation.alpha.toFixed(2) : 0}°</p>
         <p>Beta: {orientation.beta ? orientation.beta.toFixed(2) : 0}°</p>
         <p>Gamma: {orientation.gamma ? orientation.gamma.toFixed(2) : 0}°</p>
+      </div>
+      <div className="photo-container">
+        {photo && <img src={photo} alt="Captured" className="captured-photo" />}
+        <button onClick={capturePhoto} className="capture-button">
+          Capture Photo
+        </button>
       </div>
     </div>
   );
